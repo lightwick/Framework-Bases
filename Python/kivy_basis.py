ad90e8e59c0feacf9720932aa09310d321d2a3fa
LW <17444139+lightwick@users.noreply.github.com>
2021-01-17 14:01:19 +0900

    Update kivy_basis.py

--- Python/kivy_basis.py
+++ Python/kivy_basis.py
@@ -2,6 +2,31 @@ from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.core.window import Window
 
+class CanvasWidget(Widget): 
+    def __init__(self, **kwargs): 
+  
+        super(CanvasWidget, self).__init__(**kwargs) 
+  
+        # Arranging Canvas 
+        with self.canvas: 
+  
+            Color(.234, .456, .678, .8)  # set the colour  
+  
+            # Seting the size and position of canvas 
+            self.rect = Rectangle(pos = self.center, 
+                                  size =(self.width / 2., 
+                                        self.height / 2.)) 
+  
+            # Update the canvas as the screen size change 
+            self.bind(pos = self.update_rect, 
+            size = self.update_rect) 
+  
+    # update function which makes the canvas adjustable.
+    
+    def update_rect(self, *args): 
+        self.rect.pos = self.pos 
+        self.rect.size = self.size
+        
 class myApp(App):
     def build(self):
         game = Widget()
